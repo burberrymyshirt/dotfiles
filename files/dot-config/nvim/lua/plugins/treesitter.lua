d510c36ed19ceae3a8f482677e15698beb88e5f7
Magnus Larsen <magnushlarsen01@protonmail.com>
2025-06-17 16:44:52 +0200

    neovim: treesitter-textobjects and nvim-surround plugins

--- files/dot-config/nvim/lua/plugins/treesitter.lua
+++ files/dot-config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects'
+    },
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
