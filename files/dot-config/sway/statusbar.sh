059f8fea86682b305272ce8b6c5cf1e6a17d9db3
Magnus Larsen <magnushlarsen01@protonmail.com>
2025-08-03 21:20:37 +0200

    fix swaywm statusbar script showing which keyboard layout is being used
    TODO: Make a signal handler for the script, so there is no potential for
    waiting up to a second for the layout icon to change on the bar

--- files/dot-config/sway/statusbar.sh
+++ files/dot-config/sway/statusbar.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 while :; do
+    keyboard_layout=$(sb-sway-keyboard)
     battery=$(sb-battery)
     datetime=$(date +'%d-%m-%Y %H:%M:%S')
-    echo "$battery | $datetime"
+    echo "$keyboard_layout | $battery | $datetime"
     sleep 1
 done
 
