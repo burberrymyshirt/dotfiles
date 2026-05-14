e6e6e4eb0188fbf1942136d607882ecbe4c2cd92
Magnus Larsen <magnushlarsen01@protonmail.com>
2026-05-14 07:53:31 +0200

    remove blink.cmp

--- files/dot-config/nvim/init.lua
+++ files/dot-config/nvim/init.lua
@@ -79,7 +79,7 @@ vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/mason-org/mason.nvim' },
-    { src = 'https://github.com/Saghen/blink.cmp' },
+    -- { src = 'https://github.com/Saghen/blink.cmp' },
     { src = 'https://github.com/vim-scripts/cmdalias.vim' },
 })
 
